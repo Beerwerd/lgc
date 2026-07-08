0b01a52a318c62b330d4b03098be5a465fad7f74
croundio <alex.croundio@gmail.com>
2026-07-08 13:21:49 +0200

    complete arrows

--- src/games/futoshiki/components/Arrow.tsx
+++ src/games/futoshiki/components/Arrow.tsx
@@ -1,4 +1,11 @@
-import { FaAngleDown, FaAngleLeft, FaAngleRight, FaAngleUp } from 'react-icons/fa6';
+import downArrowImage from '../assets/down_arrow.png';
+import downArrowErrorImage from '../assets/down_arrow_error.png';
+import leftArrowImage from '../assets/left_arrow.png';
+import leftArrowErrorImage from '../assets/left_arrow_error.png';
+import rightArrowImage from '../assets/right_arrow.png';
+import rightArrowErrorImage from '../assets/right_arrow_error.png';
+import upArrowImage from '../assets/up_arrow.png';
+import upArrowErrorImage from '../assets/up_arrow_error.png';
 import type { ArrowDirection } from '../logic';
 
 type ArrowProps = {
@@ -7,26 +14,34 @@ type ArrowProps = {
   isInvalid?: boolean;
 };
 
-const arrowIcons = {
-  down: FaAngleDown,
-  left: FaAngleLeft,
-  right: FaAngleRight,
-  up: FaAngleUp,
+const arrowImages: Record<ArrowDirection, string> = {
+  down: downArrowImage,
+  left: leftArrowImage,
+  right: rightArrowImage,
+  up: upArrowImage,
+};
+
+const arrowErrorImages: Record<ArrowDirection, string> = {
+  down: downArrowErrorImage,
+  left: leftArrowErrorImage,
+  right: rightArrowErrorImage,
+  up: upArrowErrorImage,
 };
 
 export function Arrow({ direction, orientation, isInvalid }: ArrowProps) {
-  const Icon = direction ? arrowIcons[direction] : null;
-
   return (
     <div
       className={`futoshiki-board-mock__arrow is-${orientation}${direction ? '' : ' is-empty'}${
         isInvalid ? ' is-invalid' : ''
       }`}
     >
-      {Icon && (
-        <span className="futoshiki-board-mock__arrow-glyph">
-          <Icon />
-        </span>
+      {direction && (
+        <img
+          className={`futoshiki-board-mock__arrow-glyph is-${direction}`}
+          src={isInvalid ? arrowErrorImages[direction] : arrowImages[direction]}
+          alt=""
+          aria-hidden="true"
+        />
       )}
     </div>
   );
